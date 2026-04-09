a8b4a383fdfd3a48383af144346d1560238e7aa0
uliw <uli.wortmann@utoronto.ca>
2026-04-09 13:57:13 -0400

    Updated color scheme, and plot attribute handling

--- src/fipy-pyrite-burial/plot_layout.py
+++ src/fipy-pyrite-burial/plot_layout.py
@@ -49,20 +49,17 @@ def get_layout(df):
     # define color scheme
     poc_attr = {"color": "forestgreen", "lw": lt}
     o2_attr = {"color": "#AA4499", "lw": lt}
-
-    so4_attr = {
-        "color": "cornflowerblue",
-        # "linestyle": "dashed",
-        "lw": lt,
-        "fill_only": True,
-    }
+    so4_attr = {"color": "cornflowerblue","lw": lt,"fill_only": True}
+    so4_attr_nf = {"color": "cornflowerblue","lw": lt,"fill_only": False}
     ts2_attr = {"color": "slateblue", "zorder": 10, "lw": lt}
-    s0_attr = {"color": "forestgreen", "lw": lt}
+    s0_attr = {"color": "#009988", "lw": lt}
     fe3_attr = {"color": "saddlebrown", "lw": lt}
     fe2_s_attr = {"color": "indianred", "lw": lt}
-    fe2_l_attr = {"color": "brown", "lw": lt}
-    fes_attr = {"color": "red", "lw": lt}
+    fe2_l_attr = {"color": "#cc3311", "lw": lt}
+    fes_attr = {"color": "#ee3377", "lw": lt}
     fes2_attr = {"color": "darkorange", "lw": lt, "fill_only": True}
+    fes2_attr_nf = {"color": "darkorange", "lw": lt, "fill_only": False}
+    dbio_attr =  {"color": "#bbbbbb", "lw": lt, "fill_only": True}
 
     phi = df.phi
     # relative to sulfur
@@ -99,14 +96,14 @@ def get_layout(df):
             "yscale": "log",
             "xscale": "log",
             "ylim": (1e-6, 1e5),
-            "xlim": (4e-3, None),
+            "xlim": (4e-3, 2),
             "left_ylabel": r"Concentration [mmol/L$_{PW}$]",
             # right 1
             "right1": [
                 [fes2_fe, r"FeS$_{2}$ [wt% Fe]", fes2_attr],
                 [fes_fe * 100, r"FeS $\times$ 100 [wt% Fe]", fes_attr],
                 [fe3_fe, r"Fe$^{3+}$ [wt% Fe]", fe3_attr],
-                [fe2_fe_sorbed, r"Fe$^{2+}_{sorb}$ [wt% Fe]", fe2_s_attr],
+                # [fe2_fe_sorbed, r"Fe$^{2+}_{sorb}$ [wt% Fe]", fe2_s_attr],
                 # [
                 #     total_iron,
                 #     r"TFe [wt% Fe]",
@@ -117,9 +114,9 @@ def get_layout(df):
             "right1_ylabel": "[wt% Fe]",
             # right 2
             "right2": [
+                [s0_s, r"S$^{0}$ [wt% S]", s0_attr],
                 # [fes2_s, r"FeS$_2$ [wt% S]", {"color": "black"}],
                 # [fes_s, r"$\times$ FeS [wt% S]", {"color": "C6"}],
-                [s0_s, r"S$^{0}$ [wt% S]", s0_attr],
             ],
             "right2_ylim": (0, 4),
             "right2_ylabel": "[wt% S]",
@@ -127,45 +124,42 @@ def get_layout(df):
         "second_subplot": {
             "xaxis": [df.z, "Depth [m]"],
             "left": [
-                # [df.f_o2, "O2", {"color": "C3"}],
-                [df.f_so4, r"SO$_{4}$", so4_attr],
+                [df.f_so4, r"SO$_{4}$", so4_attr_nf],
                 [df.f_ts2, r"TS$^{2-}$", ts2_attr],
                 [df.f_fe2_total, r"Fe$^{2+}_{sorb}$", fe2_l_attr],
-                # [df.f_poc, r"POC", poc_attr],
                 [df.f_fes, "FeS", fes_attr],
                 [df.f_fe3, r"Fe${3+}$", fe3_attr],
-                [df.f_fes2, r"FeS$_{2}$", fes2_attr],
+                [df.f_fes2, r"FeS$_{2}$", fes2_attr_nf],
                 [df.f_s0, r"S$^{0}$", s0_attr],
+                [df.D_bio * 1e6, r"1e6 $\times$ D$_{bio}$ [$m^{2}/s$]", dbio_attr],
+                # [df.f_o2, "O2", {"color": "C3"}],
+                # [df.f_poc, r"POC", poc_attr],
                 # [df.f_fe2_p, "f_fe2+p", {"color": "C9"}],
-                [
-                    df.D_bio * 1000,
-                    r"D$_{bio} \times 1000$ [$m^{2}/s$]",
-                    {"color": "C8", "linestyle": "dotted"},
-                ],
             ],
-            # "right": [df.D_irr, "D_irr", {"color": "C8"}],
-            # "yscale": "symlog, linthresh=1e-14,linscale=0,1,base=10",
             "left_ylabel": r"reaction rate [mol m$^{-3}s^{-1}$]",
             "xscale": "log",
             "options-left": "set_yscale('symlog', linthresh=1e-9,linscale=0.5,base=10)",
-            "xlim": (5e-3, None),
+            "xlim": (5e-3, 2),
+            # "yscale": "symlog, linthresh=1e-14,linscale=0,1,base=10",
+            # "right": [df.D_irr, "D_irr", {"color": "C8"}],
         },
         "third_subplot": {
             "xaxis": [df.z, "Depth [m]"],
             "left": [
-                [df.d_so4, r"SO$_{4}$", so4_attr],
+                [df.d_so4, r"SO$_{4}$", so4_attr_nf],
                 [df.d_ts2, r"TS$^{2-}$", ts2_attr],
                 [df.d_fes, r"FeS", fes_attr],
-                [df.d_fes2, r"FeS$_{2}$", fes2_attr],
+                [df.d_fes2, r"FeS$_{2}$", fes2_attr_nf],
                 [df.d_s0, r"S$^{0}$", s0_attr],
             ],
-            # "right": [[df.d_h2s, "d_h2s", {"color": "C1"}]],
-            # "yscale": "log",
-            # "options-left": "set_ylim(1e-10, 1e-6)",
             "left_ylabel": r"$\delta^{34}$ [mUr VCDT]",
             "options-left": "set_ylim(-50, 100)",
             "xscale": "log",
-            "xlim": (5e-3, None),
+            "xlim": (5e-3, 2),
+            # "ylim": (-15, 75),
+            # "right": [[df.d_h2s, "d_h2s", {"color": "C1"}]],
+            # "yscale": "log",
+            "options-left": "set_ylim(-15, 75)",
         },
     }
     return plt_desc
